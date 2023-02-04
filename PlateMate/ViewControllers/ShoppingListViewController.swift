0fcd230ca4730ffe140c3c258a9231c3ca188607
coleics <cwthomps@uci.edu>
2023-02-04 12:25:34 -0800

    started shopping list view

--- PlateMate/ViewControllers/ShoppingListViewController.swift
+++ PlateMate/ViewControllers/ShoppingListViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ShoppingListViewController: UIViewController {
+class ShoppingListViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var platemateLogo: UIImageView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items = ["item 1", "item 2", "item 3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        platemateLogo.image = UIImage(named: "platemateLogo")
+        
+        self.view.addSubview(tableView)
+        
+        // Test table view
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "itemCell")
+        view.addSubview(tableView)        
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
 }
+
+
 
