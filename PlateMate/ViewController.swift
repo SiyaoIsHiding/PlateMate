265efec7dd1eb4305e32150b20bde5592e0754c2
SiyaoIsHiding <113857408+SiyaoIsHiding@users.noreply.github.com>
2023-02-03 23:11:07 -0800

    Initial Commit

---
+++ PlateMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlateMate
+//
+//  Created by HE Siyao on 3/2/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
