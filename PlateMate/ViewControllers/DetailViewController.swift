9438fad82c0e77e497f33d5151159ec73bf50e91
SiyaoIsHiding <113857408+SiyaoIsHiding@users.noreply.github.com>
2023-02-04 13:21:13 -0800

    Archi setup

---
+++ PlateMate/ViewControllers/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  PlateMate
+//
+//  Created by HE Siyao on 4/2/2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
---
+++ PlateMate/ViewControllers/TestViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  PlateMate
+//
+//  Created by HE Siyao on 4/2/2023.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func getDetail(_ sender: UIButton) {
+        print("button pressed")
+    }
+    
+}
