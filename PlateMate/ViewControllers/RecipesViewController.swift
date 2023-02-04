951ec6525428b1bd1dace0740743ee82ec5231fa
SiyaoIsHiding <113857408+SiyaoIsHiding@users.noreply.github.com>
2023-02-04 00:53:51 -0800

    update setup

---
+++ PlateMate/ViewControllers/RecipesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RecipesViewController.swift
+//  PlateMate
+//
+//  Created by HE Siyao on 3/2/2023.
+//
+
+import UIKit
+
+class RecipesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ PlateMate/ViewControllers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  PlateMate
+//
+//  Created by HE Siyao on 3/2/2023.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
