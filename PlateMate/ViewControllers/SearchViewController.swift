3519dbda4b483dea6d31b14bf860a8bfe535b3db
SiyaoIsHiding <113857408+SiyaoIsHiding@users.noreply.github.com>
2023-02-04 10:51:27 -0800

    test access

--- PlateMate/ViewControllers/SearchViewController.swift
+++ PlateMate/ViewControllers/SearchViewController.swift
@@ -13,6 +13,18 @@ class SearchViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func getRequest(_ sender: UIButton) {
+        let url = URL(string: "http://20.14.97.80")!
 
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else { return }
+            print(String(data: data, encoding: .utf8)!)
+        }
+
+        task.resume()
+    }
+    
 
 }
